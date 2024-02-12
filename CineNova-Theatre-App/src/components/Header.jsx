262a1af5942e0ba7f4dc23febef5e6c75442a3dc
KEMODEV <tristanc1015@gmail.com>
2024-02-12 00:34:50 -0500

    Finished Desktop header

--- CineNova-Theatre-App/src/components/Header.jsx
+++ CineNova-Theatre-App/src/components/Header.jsx
@@ -17,40 +17,55 @@ const Header = () => {
   return (
     <header className='flex dark:bg-[#192734] shadow-md items-center py-5 px-6'>
       <img src={isDarkMode ? darkLogo : lightLogo } alt="CineNova logo." className='w-24 h-auto' />
-      <nav>
-        <ul className='flex gap-4 text-[#192734] dark:text-white font-bold text-xl pl-16'>
-          <li>
+      <nav className='flex justify-between items-center'>
+        <ul className='flex gap-[1.1rem] text-[#192734] dark:text-white font-bold sm:text-[1.3rem] lg:text-[1.48rem] pl-16'>
+          <li className='mt-2'>
             <a href="/" className='hover:text-[#426383] transition duration-300 ease-in-out'>Home</a>
           </li>
-          <li>
+          <li className='mt-2'>
             <a href="/movies" className='hover:text-[#426383] transition duration-300 ease-in-out'>Movies</a>
           </li>
-          <li>
+          <li className='mt-2'>
             <a href="/locations" className='hover:text-[#426383] transition duration-300 ease-in-out'>Locations</a>
           </li>
-          <li>
+          <li className='mt-2'>
             <a href="/contact" className='hover:text-[#426383] transition duration-300 ease-in-out'>Contact</a>
           </li>
-          <li className='ml-5'>
+          <li className='ml-5 mt-1'>
             <button onClick={toggleTheme}> { 
-              isDarkMode ? <BsSun className="block text-white lg:text-3xl sm:text-2xl mt-1" /> : <BsMoonStars className="block lg:text-3xl sm:text-2xl mt-1" />
+              isDarkMode ? <BsSun className="block text-white text-3xl mt-1 lg:mt-2" /> : <BsMoonStars className="block text-[1.73rem] lg:text-[1.8rem] mt-1 lg:mt-2" />
             }
-            </button> 
+            </button>
           </li> 
-          <div className='flex'>
-            <li>
-              <a href="instagram.com" target='_blank'><FaInstagram className='' /></a>
-            </li>
-            <li>
-              <a href="facebook.com" target='_blank'><FaFacebook className='' /></a>
-            </li>
-            <li>
-              <a href="twitter.com" target='_blank'><FaTwitter className='' /></a>
-            </li>
-          </div>
         </ul>
       </nav>
+      <div className='flex justify-between items-end w-[100%]'>
+      <ul className='flex justify-end gap-3 ml-auto'>
+        <li>
+          <div className='md:h-14 md:w-14 lg:h-16 lg:w-16 2xl:h-[4.5rem] 2xl:w-[4.5rem] bg-[#192734] rounded-[50%] flex justify-center items-center z-0 dark:bg-white'>
+            <a href="instagram.com" target='_blank'>
+              <FaInstagram className='text-white lg:w-11 2xl:w-12 md:w-[2.4rem] h-auto z-10 dark:md:w-10 dark:lg:w-12 dark:2xl:w-14 dark:text-[#192734]' />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div className='md:h-14 md:w-14 lg:h-16 lg:w-16 2xl:h-[4.5rem] 2xl:w-[4.5rem] bg-[#192734] rounded-[50%] flex justify-center items-center z-0 dark:bg-white'>
+            <a href="facebook.com" target='_blank'>
+              <FaFacebook className='text-white lg:w-11 2xl:w-12 md:w-[2.42rem] h-auto z-10 dark:lg:w-[2.85rem] dark:text-[#192734]' />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div className='md:h-14 md:w-14 lg:h-16 lg:w-16 2xl:h-[4.5rem] 2xl:w-[4.5rem] bg-[#192734] rounded-[50%] flex justify-center items-center z-0 dark:bg-white'>
+            <a href="twitter.com" target='_blank'>
+              <FaTwitter className='text-white lg:w-11 2xl:w-12 md:w-9 h-auto z-10 dark:text-[#192734]' />
+            </a>
+          </div>
+        </li>
+      </ul>
+      </div>
     </header>
+    
   );
 };
 
