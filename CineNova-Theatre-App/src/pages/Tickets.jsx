9431710cf8841727b8d8049304149deb764b8f66
KEMODEV <tristanc1015@gmail.com>
2024-04-05 00:37:27 -0400

    Tickets page design is almost finalized
    Tickets page seems fine except for possibly adding background colors to the plus and minus buttons. And of course need to add the seat selector display after clicking button.

--- CineNova-Theatre-App/src/pages/Tickets.jsx
+++ CineNova-Theatre-App/src/pages/Tickets.jsx
@@ -1,23 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Tickets.css'
 
 const Tickets = (props) => {
 
-  let value = 0;
 
   let [childTicketCount, setChildTicketCount] = useState(0);
-  let [adultTicketCount, setAdultTicketCount] = useState(0);
+  let [generalTicketCount, setGeneralTicketCount] = useState(0);
   let [seniorTicketCount, setSeniorTicketCount] = useState(0);
+  let [totalTicketCount, setTotalTicketCount] = useState(0);
+  useEffect(() => {
+    const reserveBtn = document.querySelector('#reserve-seats');
+    console.log(reserveBtn);
+    if (childTicketCount > 0 || generalTicketCount > 0 || seniorTicketCount > 0) {
+      reserveBtn.style.background = "#fb4242";
+      reserveBtn.style.cursor = "pointer";
+    } else {
+      reserveBtn.style.background = "#6c757de1";
+      reserveBtn.style.cursor = "default";
+    }
+  })
+  
 
   function handleOperator(elem) {
     if (elem.target.className.includes("plus-btn")) {
-      if (childTicketCount !== 10 && adultTicketCount !== 10 && seniorTicketCount !== 10) {
+      if (childTicketCount !== 10 && generalTicketCount !== 10 && seniorTicketCount !== 10) {
         switch(true) {
           case elem.target.className.includes('child-ticket'):
             setChildTicketCount(childTicketCount += 1);
             break;
-          case elem.target.className.includes('adult-ticket'):
-            setAdultTicketCount(adultTicketCount += 1);
+          case elem.target.className.includes('general-ticket'):
+            setGeneralTicketCount(generalTicketCount += 1);
             break;
           case elem.target.className.includes('senior-ticket'):
             setSeniorTicketCount(seniorTicketCount += 1);
@@ -27,14 +39,13 @@ const Tickets = (props) => {
         }
       }
     } else if (elem.target.className.includes("minus-btn")) {
-      if (childTicketCount !== 0 || adultTicketCount !== 0 || seniorTicketCount !== 0) {
-        console.log(elem.target.className.includes("minus-btn"));
+      if (childTicketCount !== 0 || generalTicketCount !== 0 || seniorTicketCount !== 0) {
         switch(true) {
           case elem.target.className.includes('child-ticket'):
             setChildTicketCount(childTicketCount -= 1);
             break;
-          case elem.target.className.includes('adult-ticket'):
-            setAdultTicketCount(adultTicketCount -= 1);
+          case elem.target.className.includes('general-ticket'):
+            setGeneralTicketCount(generalTicketCount -= 1);
             break;
           case elem.target.className.includes('senior-ticket'):
             setSeniorTicketCount(seniorTicketCount -= 1);
@@ -44,21 +55,19 @@ const Tickets = (props) => {
         }
       }
     } else {
-      console.log("button classes error")
+      console.log("button classes error");
     }
+    setTotalTicketCount(totalTicketCount = childTicketCount + generalTicketCount + seniorTicketCount);
+    
   }
 
-  /*const handlePlus = (elem) => {
-    value+=1;
-    console.log(elem.target.className);
-  }
-  const handleMinus = (elem) => {
-    value-=1;
-    console.log(elem.target.className.includes("minus-btn"));
-  }*/
   const handleChange = (event) => {
+    setChildTicketCount(Number(event.target.value));
+    setGeneralTicketCount(Number(event.target.value));
+    setSeniorTicketCount(Number(event.target.value));
     Number(event.target.value);
   }
+
 
   return (
     <main className='size-full pb-20 mt-[10.53rem] sm:mt-[11.5rem] md:mt-[12.5rem] xl:mt-[13rem] mobile-menu:mt-[12.53rem]'>
@@ -75,17 +84,21 @@ const Tickets = (props) => {
         <hr className='box-content border-[#192734bd] dark:border-[#ffff00] border-t-2 border-b-2 text-[1.2rem] px-44 md:px-48 mt-3 rounded-[6px] dark:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_10px_#FFFF00,0_0_20px_#FFFF00,0_0_40px_#FFFF00]' />
       </div>
 
-      <article className='grid grid-cols-1 sm:grid-cols-2 mt-16 mobile-menu:mx-8 max-w-[90rem] place-self-center place-items-center gap-x-10 gap-y-3'>
-        <div 
-          className='w-56 sm:w-60 lg:w-80 h-auto border-2 border-gray-100' 
+      <article className='grid grid-cols-1 sm:grid-cols-2 mt-16 mobile-menu:mx-8 xl:mx-28 2xl:mx-56 place-items-center gap-x-10 gap-y-3'>
+
+        <section 
+          className='w-56 sm:w-60 mobile-menu:w-80 h-auto' 
         >
           <img
             src={`${localStorage.getItem("movieImg")}`} 
-            className='w-full h-full'
+            className='w-full h-full border-2 border-gray-100'
           />
-        </div>
+          <h1 className='m-3 dark:text-white text-4xl font-bold text-center'>
+            {localStorage.getItem("movieTitle")}
+          </h1>
+        </section>
 
-        <section>
+        <section className='mt-12 sm:mt-0'>
 
           {/* Child tickets */}
           <p className='text-[#192734] dark:text-white text-2xl font-semibold'>Child {'('}0-12yo{')'}</p>
@@ -98,7 +111,7 @@ const Tickets = (props) => {
             </button>
             <input 
               type="number" 
-              className="ticket-count" 
+              className="ticket-count"
               onChange={handleChange}
               value={childTicketCount} 
             />
@@ -109,13 +122,14 @@ const Tickets = (props) => {
               +
             </button>
           </div>
-
+          <aside className='relative top-[-2.5rem] text-md font-semibold text-[#333333be] dark:text-[#b3b3b3]'>$3.99 per ticket</aside>
           
-          {/* Adult tickets */}
+
+          {/* General tickets */}
           <p className='text-[#192734] dark:text-white text-2xl font-semibold'>General {'('}13-59yo{')'}</p>
           <div className='wrapper'>
             <button 
-              className="plus-minus minus-btn adult-ticket" 
+              className="plus-minus minus-btn general-ticket" 
               onClick={handleOperator}
             >
               -
@@ -123,17 +137,18 @@ const Tickets = (props) => {
             <input 
               type="number" 
               className="ticket-count" 
-              onChange={handleChange} 
-              value={adultTicketCount} 
+              onChange={handleChange}
+              value={generalTicketCount} 
             />
             <button 
-              className="plus-minus plus-btn adult-ticket" 
+              className="plus-minus plus-btn general-ticket" 
               onClick={handleOperator}
             >
               +
             </button>
           </div>
-          
+          <aside className='relative top-[-2.5rem] text-md font-semibold text-[#333333be] dark:text-[#b3b3b3]'>$8.99 per ticket</aside>
+
 
           {/* Senior tickets */}
           <p className='text-[#192734] dark:text-white text-2xl font-semibold'>Senior {'('}60yo-{')'}</p>
@@ -147,7 +162,7 @@ const Tickets = (props) => {
             <input 
               type="number" 
               className="ticket-count" 
-              onChange={handleChange} 
+              onChange={handleChange}
               value={seniorTicketCount} 
             />
             <button 
@@ -157,18 +172,14 @@ const Tickets = (props) => {
               +
             </button>
           </div>
+          <aside className='relative top-[-2.5rem] text-md font-semibold text-[#333333be] dark:text-[#b3b3b3]'>$5.99 per ticket</aside>
         </section>
-
-        <section className=''>
-          <h2 className='m-3 dark:text-white text-3xl font-semibold'>
-            {localStorage.getItem("movieTitle")}
-          </h2>
-        </section>
-
-        <section>
+        
+        <section className='relative top-[-5rem] lg:top-[-4rem] sm:col-span-2'>
           <button 
             type='submit' 
-            className='reserve-seats cursor-default'
+            id='reserve-seats'
+            className='cursor-default transition duration-500 ease-in-out'
           >
             Reserve Seats
           </button>
